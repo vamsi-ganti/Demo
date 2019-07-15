caefe547664f8f1c889b0bab5dbfb8675eb05f61
vamsi-ganti <vamc.ganti@gmail.com>
2019-07-15 23:33:54 +0530

    Changed Details Page

--- Details.aspx.cs
+++ Details.aspx.cs
@@ -12,6 +12,7 @@ namespace WebApplication1
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack) { txtName.Focus(); }
+			//hello akkadunavu//
 
 
         }
